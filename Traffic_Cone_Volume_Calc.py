ffd0ded209493f31bd3aa6ade8ce12e27bc58ca3
JasolaCode <joshsolano56@gmail.com>
2023-09-28 11:44:16 +1000

    initial changes for Cone class

--- Traffic_Cone_Volume_Calc.py
+++ Traffic_Cone_Volume_Calc.py
@@ -3,46 +3,61 @@ room_length = int(input('Enter the length of your room in cm:'))
 room_width = int(input('Enter the width of your room in cm:'))
 room_height = int(input('Enter the height of your room in cm:'))
 
-# declare cone size (might add variable sizing later for small, medium, large cones)
-cone_length = float(36.0)
-cone_width = float(36.0)
-cone_height = float(70.0)
-cone_stack_gap = float(7.6)
 
-def calc_volume(length, width, height): # Calculates Room Volume
+# declare cone object with size (for small, medium, large cones)
+class Cone:
+    def __init__(self, length, width, height, stack_gap):
+        self.length = length
+        self.width = width
+        self.height = height
+        self.stack_gap = stack_gap
+
+    def __print__(self):
+        return f'--- Cone Dimensions --- /nLength: {self.length} /nWidth: {self.width} /nHeight: {self.height}'
+
+
+def calc_volume(length, width, height):  # Calculates Object Volume
     return length * width * height
 
-def calc_stack_attr(height, gap, room_height): #Calculates the stack heaight of cones if higher than ceiling
-    stack_height = float(0.0) # Declare for cone stack height
-    num_cones = int(0) # use to calculate number of cones in one stack
-    while stack_height <= room_height: # While the cone stack hasn't hit the ceiling yet
-        if (num_cones == 0): # Calculations for the first cone are different
+
+def calc_stack_attr(height, gap, room_height):  # Calculates the stack heaight of cones if higher than ceiling
+    stack_height = float(0.0)  # Declare for cone stack height
+    num_cones = int(0)  # use to calculate number of cones in one stack
+    while stack_height <= room_height:  # While the cone stack hasn't hit the ceiling yet
+        if (num_cones == 0):  # Calculations for the first cone are different
             if ((height + gap) > room_height):
                 return [num_cones, stack_height, room_height]
             else:
-                stack_height += height # the first cone creates a stack height of cone_height
+                stack_height += height  # the first cone creates a stack height of cone_height
         else:
-            if ((stack_height + gap) <= room_height): # if the next cone is below the ceiling
+            if ((stack_height + gap) <= room_height):  # if the next cone is below the ceiling
                 stack_height += gap
             else:
-                return [num_cones, stack_height, room_height] # if the next cone will be higher than the ceiling
-        num_cones += 1 # when all else checks out... add a cone
+                return [num_cones, stack_height, room_height]  # if the next cone will be higher than the ceiling
+        num_cones += 1  # when all else checks out... add a cone
 
     return 'bleh, error'
 
-def stacks_in_room(room_volume, stack_volume): # Calculate the amount of stacks in a room
+
+def stacks_in_room(room_volume, stack_volume):
     return room_volume / stack_volume
 
 
-room_volume = calc_volume(room_length, room_width, room_height) # Set room volume as variable
+cone = Cone(36.0, 36.0, 70, 7.6)
 
-print(f'The volume of your room is {room_volume}') # currently using for testing purposes
+volume = lambda length, width, height: length * width * height  # Lambda function, set volume as variable
 
-cone_stack_attr = calc_stack_attr(cone_height, cone_stack_gap, room_height) # Set stack attributes as variable
+print(
+    f'The volume of your room is {volum(room_length, room_width, room_height)}')  # currently using for testing purposes
 
-print(f'The number of cones that can fit in one stack is {cone_stack_attr[0]}. This has a stack height of {cone_stack_attr[1]}, with {round(cone_stack_attr[2] - cone_stack_attr[1], 2)}cm remaining until hitting the ceiling.') # currently using for testing purposescurrently using for testing purposes
+cone_stack_attr = calc_stack_attr(cone.height, cone.stack_gap, room_height)  # Set stack attributes as variable
+
+print(
+    f'The number of cones that can fit in one stack is {cone_stack_attr[0]}. This has a stack height of {cone_stack_attr[1]}, with {round(cone_stack_attr[2] - cone_stack_attr[1], 2)}cm remaining until hitting the ceiling.')  # currently using for testing purposescurrently using for testing purposes
 
 result = stacks_in_room(room_volume, calc_volume(cone_length, cone_width, cone_stack_attr[2]))
-print (f'the amount of stacks you can fit in the room is {round(result)}')
+print(f'the amount of stacks you can fit in the room is {round(result)}')
 
 print(f'total number of cones is {cone_stack_attr[0] * round(result)}')
+
+print(cone.print)
